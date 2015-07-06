172d289d99a865cdff34c4bbea9fb0cbffb64bac
Oliver Silva Pérez <oliver.silvaperez@gmail.com>
2015-07-06 21:46:03 +0100

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 // rutas de quizes
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
