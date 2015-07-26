c0441f4e9e79afa22d511b1ea9c7034f447ea7d5
Oliver Silva Pérez <oliver.silvaperez@gmail.com>
2015-07-26 11:36:58 +0100

    Crear pregunta

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,10 @@ router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
+
+
 
 router.get('/author', function(req, res, next) {
 res.render('author', {title: 'Quiz',author: 'Oliver Silva Pérez'});
